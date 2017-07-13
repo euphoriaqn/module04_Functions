12d3d74303beb97f595fdfa733b5bc588c4fff05
euphoriaqn <euphoriaqn@gmail.com>
2017-07-13 13:29:38 +0300

    Допрацьоване завдання № 4

--- src/exerciseFour/exerciseFour.java
+++ src/exerciseFour/exerciseFour.java
@@ -10,11 +10,66 @@ import java.util.Scanner;
 public class exerciseFour {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите целое число X:");
-        int x = sc.nextInt();
-        System.out.println("Введите целое число Y:");
-        int y = sc.nextInt();
+        System.out.println("Які числа ви будете вводити? Цілі чи дробні?   1 - цілі, 2 - дробні");
+        int choice = sc.nextInt();
+        switch(choice) {
+            // Логіка коли користувач вибрав цілі числа
+            case 1:
+                int intX;
+                int intY;
+                while (true) {
+                    try {
+                        System.out.println("Введите целое число X:");
+                        intX = sc.nextInt();
+                        sc.nextLine();
+                        break;
+                    } catch (Exception e) {
+                        sc.nextLine();
+                    }
+                }
+                while (true) {
+                    try {
+                        System.out.println("Введите целое число Y:");
+                        intY = sc.nextInt();
+                        sc.nextLine();
+                        break;
+                    } catch (Exception e) {
+                        sc.nextLine();
+                    }
+                }
+                int maxInt = getMax(intX, intY);
+                System.out.println("Максимальне значення серед цілих чисел " + intX + " та " + intY + " буде " + maxInt );
+                break;
+            // Логіка коли користувач вибрав дробні числа
+            case 2:
+                double doubleX;
+                double doubleY;
+                while (true) {
+                    try {
+                        System.out.println("Введите дробное число X:");
+                        doubleX = sc.nextDouble();
+                        sc.nextLine();
+                        break;
+                    } catch (Exception e) {
+                        sc.nextLine();
+                    }
+                }
+                while (true) {
+                    try {
+                        System.out.println("Введите дробное число Y:");
+                        doubleY = sc.nextDouble();
+                        sc.nextLine();
+                        break;
+                    } catch (Exception e) {
+                        sc.nextLine();
+                    }
+                }
+                double maxDouble = getMax(doubleX, doubleY);
+                System.out.println("Максимальне значення серед цілих чисел " + doubleX + " та " + doubleY + " буде " + maxDouble );
+                break;
+        }
     }
+    //Функція визначення максимального числа серед цілих чисел
     public static int getMax(int x, int y){
         int max;
         if (x>y)
@@ -23,6 +78,7 @@ public class exerciseFour {
             max = y;
         return max;
     }
+    //Функція вихначення максимального числа серед дробних чисел
     public static double getMax(double x, double y){
         double max;
         if (x>y)
